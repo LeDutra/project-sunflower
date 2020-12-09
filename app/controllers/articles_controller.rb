622bec7cab1418a942e4aa6417597d20f575e07b
Leonardo Dutra <leo.togepi@gmail.com>
2020-12-09 18:23:31 -0300

    implmtd error messages and devise gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,53 +1,50 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
-    http_basic_authenticate_with name: "lddutra", password: "027781", except: [:index, :show]
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def index
-        @articles = Article.all
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
     end
+  end
 
-    def show
-        @article = Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render "edit"
     end
+  end
 
-    def new
-        @article = Article.new
-    end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+    redirect_to articles_path
+  end
 
-    def create
-        @article = Article.new(article_params)
+  private
 
-        if @article.save
-            redirect_to @article
-        else
-            render 'new'
-        end
-    end
-
-    def update
-        @article = Article.find(params[:id])
-
-        if @article.update(article_params)
-            redirect_to @article
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-
-        redirect_to articles_path
-    end
-
-
-    private
-        def article_params
-            params.require(:article).permit(:title, :text)
-        end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
